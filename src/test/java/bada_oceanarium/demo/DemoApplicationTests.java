f8e5f3fffa57fdf13db461ebea9348ef13f39bde
bartosz.ziemba <bartosz.ziemba@radrat.pl>
2023-12-27 20:40:18 +0100

    init

---
+++ src/test/java/bada_oceanarium/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bada_oceanarium.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
