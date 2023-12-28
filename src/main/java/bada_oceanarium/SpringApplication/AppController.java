554dff03ccf669d512746e20a6b8a048169ca26e
Bartosz <bartoszzziemba@gmail.com>
2023-12-28 20:35:24 +0100

    koniec 3 czesci walkthrough

--- src/main/java/bada_oceanarium/SpringApplication/AppController.java
+++ src/main/java/bada_oceanarium/SpringApplication/AppController.java
@@ -3,8 +3,14 @@ package bada_oceanarium.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Configuration
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AppController implements WebMvcConfigurer {
@@ -13,5 +19,33 @@ public class AppController implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/main").setViewName("main");
         registry.addViewController("/login").setViewName("login");
+
+        registry.addViewController("/main_admin").setViewName("admin/main_admin");
+        registry.addViewController("/main_user").setViewName("user/main_user");
+    }
+
+    @Controller
+    public class DashboardController {
+        @RequestMapping("/main")
+        public String defaultAfterLogin(HttpServletRequest request) {
+            if (request.isUserInRole("ADMIN")) {
+                return "redirect:/main_admin";
+            }
+            else if (request.isUserInRole("USER")) {
+                return "redirect:/main_user";
+            }
+            else {
+                return "redirect:/index";
+            }
+        }
+    }
+
+    @RequestMapping(value={"/main_admin"})
+    public String showAdminPage(Model model) {
+        return "admin/main_admin";
+    }
+    @RequestMapping(value={"/main_user"})
+    public String showUserPage(Model model) {
+        return "user/main_user";
     }
 }
